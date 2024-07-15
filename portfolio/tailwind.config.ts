a28d3e8f0ecdcabb51801b46acc80b1dfa8ad640
Annah-2003 <irene.gitau@strathmore.edu>
2024-07-15 06:52:02 -0700

    created pages folder

--- portfolio/tailwind.config.ts
+++ portfolio/tailwind.config.ts
@@ -1,20 +1,20 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',    // Include all relevant files in the pages directory
+    './components/**/*.{js,ts,jsx,tsx,mdx}', // Include all relevant files in the components directory
+    './app/**/*.{js,ts,jsx,tsx,mdx}',       // Include all relevant files in the app directory
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
 };
+
 export default config;
