da9d3fd1329a1263142908ecfbc8390609eb25ff
Annah-2003 <irene.gitau@strathmore.edu>
2024-07-16 22:00:30 -0700

    installed react icons

--- portfolio/components/NavBar.js
+++ portfolio/components/NavBar.js
@@ -1,4 +1,16 @@
-import Link from 'next/link'
-import styled from 'styled-components';
+// components/Navbar.js
+import Link from 'next/link';
 
-const Nav = styled.nav`
+const Navbar = () => (
+  <nav className="bg-gray-900 text-white p-4 flex justify-between items-center">
+    <div className="text-2xl font-bold">My Portfolio</div>
+    <div className="space-x-6">
+      <Link href="/" className="hover:text-blue-500">Home</Link>
+      <Link href="/about" className="hover:text-blue-500">About</Link>
+      <Link href="/projects" className="hover:text-blue-500">Projects</Link>
+      <Link href="/contact" className="hover:text-blue-500">Contact</Link>
+    </div>
+  </nav>
+);
+
+export default Navbar;
