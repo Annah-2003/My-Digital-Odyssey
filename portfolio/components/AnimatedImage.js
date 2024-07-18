ffe6abf118287dcc336124db1d7e46684af8245f
Annah-2003 <irene.gitau@strathmore.edu>
2024-07-17 22:15:22 -0700

    created contact component

---
+++ portfolio/components/AnimatedImage.js
@@ -0,0 +1,15 @@
+import {motion} from 'framer-motion';
+
+
+const AnimatedImage = ({src, alt}) => (
+    <motion.img
+    src={src}
+    alt={alt}
+    initial={{opacity: 0, y: 50}}
+    animate={{opacity: 1, y: 0 }}
+    transition= {{duration: 1}}
+    className= "w-full h-auto"
+    />
+);
+
+export default AnimatedImage;
