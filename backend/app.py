f6124d226ab30cd4b95213294459a61bfca6ab9d
Annah-2003 <irene.gitau@strathmore.edu>
2024-07-25 09:50:30 -0700

    modified app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,19 @@
- 
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route ('/contact', methods=['POST'])
+def contact():
+    data= request.get_json()
+    name = data.get('name')
+    email = data.get('email')
+    message = data.get('message')
+
+    print(f"Received message from {name ({email}):{message}}")
+    return jsonify({"message": "Message received successfully!"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
